51a84dcc4d803518e43a487094b2842a7523d4c2
kahzukie <igorcruzoliveira@hotmail.com>
2024-12-07 11:42:40 -0300

    fix: eidos now will be all in one json separated with eido name on the json

--- scrapeStatus.py
+++ scrapeStatus.py
@@ -3,6 +3,7 @@ import time as t
 from bs4 import BeautifulSoup
 from lxml import etree
 import pandas as pd
+import json
 
 
 def scrapEidos(url):
@@ -21,6 +22,7 @@ def scrapEidos(url):
 def scrapStats(urlEidos):
     # This will scrape the stat from the eido (which is a table that's why
     # pandas), and return it as json
+    all_eidos_data = []
     for url in urlEidos:
         # This gets the urls from scrapEidos and paste them finding
         # the table we want
@@ -29,20 +31,29 @@ def scrapStats(urlEidos):
         # This just find the table we whant :)
         soup = BeautifulSoup(response.text, 'html.parser')
         table = soup.find('th', string='Wish').parent.parent.parent
-        # The sleep is there just so we don't spam the website
-        t.sleep(2)
 
         # Using pandas so it reads the html and fixes it in table to convert to json
         df = pd.read_html(str(table))[0]
+        # Using this to save eido name extracted from the URL
+        eido_name = url.split('/')[-1]
+        # Add eido name to dataframe
+        df['Eido Name'] = eido_name
         # This converts the table to csv
         # df.to_csv(f"eido_stats_{url.split('/')[-1]}.csv", index=False)
 
         # This converts the table to json
         json_data = df.to_json(orient='records')
 
-        with open(f"eido_stats_{url.split('/')[-1]}.json", 'w') as f:
-            f.write(json_data)
-        print(f"Stats for {url.split('/')[-1]} extracted!")
+        # Append all eidos to one json
+        all_eidos_data.extend(json.loads(json_data))
+        # Save all eidos into one json
+        with open('all_eidos_stats.json', 'w') as f:
+            json.dump(all_eidos_data, f, indent=4)
+        # Prints te result when all eidos stats were extracted
+        print("All eido stats extracted and combined!")
+
+        # Same but for each eido
+        # print(f"Stats for {url.split('/')[-1]} extracted!")
 
 
 if __name__ == '__main__':
